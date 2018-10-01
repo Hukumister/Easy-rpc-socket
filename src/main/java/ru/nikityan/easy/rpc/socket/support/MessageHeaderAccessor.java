b14bb991cc8a6964d4fa4723db6c1e8700bca1e6
nmzaltsman <Zaltsman.Nikita@gmail.com>
2018-10-02 00:45:43 +0700

    * add method for message type to MessageHeaderAccessor

--- src/main/java/ru/nikityan/easy/rpc/socket/support/MessageHeaderAccessor.java
+++ src/main/java/ru/nikityan/easy/rpc/socket/support/MessageHeaderAccessor.java
@@ -4,6 +4,7 @@ import org.jetbrains.annotations.Nullable;
 import org.springframework.util.Assert;
 import ru.nikityan.easy.rpc.socket.Message;
 import ru.nikityan.easy.rpc.socket.MessageHeaders;
+import ru.nikityan.easy.rpc.socket.MessageType;
 
 import java.util.Map;
 
@@ -18,11 +19,19 @@ public class MessageHeaderAccessor {
 
     private final MuttableHeaders messageHeaders;
 
+    private MessageHeaderAccessor(MessageHeaders messageHeaders) {
+        this.messageHeaders = new MuttableHeaders(messageHeaders);
+    }
+
     private MessageHeaderAccessor(Message<?> message) {
         this.messageHeaders = new MuttableHeaders(message.getMessageHeader());
     }
 
-    public static MessageHeaderAccessor createAccessor(Message<?> message) {
+    public static MessageHeaderAccessor ofHeaders(MessageHeaders messageHeaders) {
+        return new MessageHeaderAccessor(messageHeaders);
+    }
+
+    public static MessageHeaderAccessor ofMessage(Message<?> message) {
         Assert.notNull(message, "message required");
         return new MessageHeaderAccessor(message);
     }
@@ -36,12 +45,29 @@ public class MessageHeaderAccessor {
         return null;
     }
 
+    @Nullable
+    public static String getMessageMethod(MessageHeaders messageHeaders) {
+        Object messageMethod = messageHeaders.get(MESSAGE_METHOD);
+        if (messageMethod != null) {
+            return (String) messageMethod;
+        }
+        return null;
+    }
+
+    public void setMessageType(MessageType messageType) {
+        this.messageHeaders.getRawHeader().put(MessageHeaders.MESSAGE_TYPE, messageType);
+    }
+
     public void setMessageMethod(String method) {
         this.messageHeaders.getRawHeader().put(MESSAGE_METHOD, method);
     }
 
     public void setSubscribeName(String method) {
         this.messageHeaders.getRawHeader().put(SUBSCRIBE_METHOD, method);
+    }
+
+    public MessageType messageType() {
+        return (MessageType) this.messageHeaders.getRawHeader().get(MessageHeaders.MESSAGE_TYPE);
     }
 
     public String getMessageMethod() {
