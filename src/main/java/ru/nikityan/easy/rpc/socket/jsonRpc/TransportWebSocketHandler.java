a40419ceb92182080479b721765cd95782aa6a6d
nmzaltsman <Zaltsman.Nikita@gmail.com>
2018-10-03 20:51:15 +0700

    + add TransportWebSocketHandler

---
+++ src/main/java/ru/nikityan/easy/rpc/socket/jsonRpc/TransportWebSocketHandler.java
@@ -0,0 +1,231 @@
+package ru.nikityan.easy.rpc.socket.jsonRpc;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.SmartLifecycle;
+import org.springframework.util.Assert;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.AbstractWebSocketHandler;
+import org.springframework.web.socket.handler.ConcurrentWebSocketSessionDecorator;
+import ru.nikityan.easy.rpc.socket.Message;
+import ru.nikityan.easy.rpc.socket.MessageChannel;
+import ru.nikityan.easy.rpc.socket.MessageHandler;
+import ru.nikityan.easy.rpc.socket.SubscribeMessageChanel;
+import ru.nikityan.easy.rpc.socket.core.JsonRpcIncomingConverter;
+import ru.nikityan.easy.rpc.socket.core.MessageConverter;
+import ru.nikityan.easy.rpc.socket.exceptions.JsonRequestException;
+import ru.nikityan.easy.rpc.socket.exceptions.MessagingException;
+import ru.nikityan.easy.rpc.socket.support.MessageBuilder;
+import ru.nikityan.easy.rpc.socket.support.MessageHeaderAccessor;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+/**
+ * Created by Nikit on 02.10.2018.
+ */
+public class TransportWebSocketHandler extends AbstractWebSocketHandler implements MessageHandler, SmartLifecycle {
+
+    protected Logger logger = LoggerFactory.getLogger(getClass());
+
+    private final Gson gson = new GsonBuilder().serializeNulls().create();
+
+    private final Map<String, WebSocketSession> sessions = new ConcurrentHashMap<>();
+
+    private final Map<String, Set<WebSocketSession>> subscribers = new ConcurrentHashMap<>();
+
+    private final MessageConverter messageConverter = new JsonRpcIncomingConverter();
+
+    private int sendTimeLimit = 10 * 1000;
+
+    private int sendBufferSizeLimit = 512 * 1024;
+
+    private volatile boolean running = false;
+
+    private final Object lifecycleMonitor = new Object();
+
+    private final SubscribeMessageChanel clientInboundChannel;
+
+    private final MessageChannel clientOutboundChannel;
+
+    public TransportWebSocketHandler(SubscribeMessageChanel clientInboundChannel, MessageChannel clientOutboundChannel) {
+        Assert.notNull(clientInboundChannel, "Inbound MessageChannel must not be null");
+        Assert.notNull(clientInboundChannel, "Outbound MessageChannel must not be null");
+
+        this.clientOutboundChannel = clientOutboundChannel;
+        this.clientInboundChannel = clientInboundChannel;
+    }
+
+    @Override
+    public boolean isAutoStartup() {
+        return false;
+    }
+
+    @Override
+    public void stop(Runnable callback) {
+        synchronized (this.lifecycleMonitor) {
+            stop();
+            if (callback != null) {
+                callback.run();
+            }
+        }
+    }
+
+    @Override
+    public void start() {
+        synchronized (this.lifecycleMonitor) {
+            this.clientInboundChannel.subscribe(this);
+            this.running = true;
+        }
+    }
+
+    @Override
+    public void stop() {
+        synchronized (this.lifecycleMonitor) {
+            this.running = false;
+            this.clientInboundChannel.unSubscribe(this);
+        }
+
+        for (WebSocketSession socketSession : this.sessions.values()) {
+            try {
+                socketSession.close(CloseStatus.GOING_AWAY);
+            } catch (Throwable ex) {
+                logger.warn("Failed to close {}", socketSession, ex);
+            }
+        }
+    }
+
+    @Override
+    public boolean isRunning() {
+        return running;
+    }
+
+    @Override
+    public int getPhase() {
+        return Integer.MAX_VALUE;
+    }
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        if (!session.isOpen()) {
+            return;
+        }
+        WebSocketSession webSocketSession = decorateSession(session);
+        this.sessions.put(webSocketSession.getId(), webSocketSession);
+        logger.debug("Open connection webSocket session, sessionId = {}", session.getId());
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        try {
+            Message<?> sendMessage = messageConverter.toMessage(message.getPayload());
+            MessageHeaderAccessor accessor = MessageHeaderAccessor.ofMessage(sendMessage);
+            accessor.setSessionId(session.getId());
+            sendMessage = MessageBuilder.fromMessage(sendMessage)
+                    .withHeaders(accessor.getMessageHeaders())
+                    .build();
+            boolean result = this.clientOutboundChannel.send(sendMessage);
+            if (!result) {
+                logger.warn("Fail sent message to message chanel message = {}", message);
+            }
+        } catch (JsonRequestException ex) {
+            handleFailRequest(session, ex);
+        }
+    }
+
+    private void handleFailRequest(WebSocketSession session, JsonRequestException exception) {
+        JsonRpcRequest request = exception.getOriginalRequest();
+        JsonRpcResponse rpcResponse;
+        if (request != null) {
+            rpcResponse = new JsonRpcResponse(request.getId(), exception.getRpcError());
+        } else {
+            rpcResponse = new JsonRpcResponse(-1L, exception.getRpcError());
+        }
+        Message<JsonRpcResponse> message = MessageBuilder.fromPayload(rpcResponse).build();
+        sendMessage(session, message);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus closeStatus) throws Exception {
+        logger.debug("Close connection, webSocket sessionId = {}, close status = {}", session.getId(), closeStatus);
+        this.sessions.remove(session.getId());
+    }
+
+    @Override
+    public boolean supportsPartialMessages() {
+        return false;
+    }
+
+    @Override
+    public void handleMessage(Message<?> message) throws MessagingException {
+        String sessionId = MessageHeaderAccessor.getSessionId(message.getMessageHeader());
+        WebSocketSession webSocketSession = sessions.get(sessionId);
+
+        String subscribeMethod = MessageHeaderAccessor.getSubscribeMethod(message.getMessageHeader());
+        String sendMessageMethod = MessageHeaderAccessor.getSendMessageMethod(message.getMessageHeader());
+
+        if (subscribeMethod != null) {
+            if (!subscribers.containsKey(subscribeMethod)) {
+                subscribers.put(subscribeMethod, new CopyOnWriteArraySet<>());
+            }
+            subscribers.get(subscribeMethod).add(webSocketSession);
+            sendMessage(webSocketSession, message);
+            return;
+        }
+
+        if (sendMessageMethod != null) {
+            broadCastMessage(sendMessageMethod, message);
+        } else {
+            sendMessage(webSocketSession, message);
+        }
+    }
+
+    private void sendMessage(WebSocketSession socketSession, Message<?> message) {
+        try {
+            Object payload = message.getPayload();
+            String json = gson.toJson(payload);
+            TextMessage textMessage = new TextMessage(json);
+            socketSession.sendMessage(textMessage);
+        } catch (IOException ex) {
+            logger.warn("Error while try to send text message session = {}, message = {}", socketSession, message);
+        }
+    }
+
+    private void broadCastMessage(String method, Message<?> message) {
+        Set<WebSocketSession> sessions = subscribers.get(method);
+        if (sessions == null) {
+            logger.debug("Session set is null");
+            return;
+        }
+        for (WebSocketSession socketSession : sessions) {
+            sendMessage(socketSession, message);
+        }
+    }
+
+    protected WebSocketSession decorateSession(WebSocketSession session) {
+        return new ConcurrentWebSocketSessionDecorator(session, getSendTimeLimit(), getSendBufferSizeLimit());
+    }
+
+    public int getSendTimeLimit() {
+        return sendTimeLimit;
+    }
+
+    public void setSendTimeLimit(int sendTimeLimit) {
+        this.sendTimeLimit = sendTimeLimit;
+    }
+
+    public int getSendBufferSizeLimit() {
+        return sendBufferSizeLimit;
+    }
+
+    public void setSendBufferSizeLimit(int sendBufferSizeLimit) {
+        this.sendBufferSizeLimit = sendBufferSizeLimit;
+    }
+}
---
+++ src/test/java/ru/nikityan/easy/rpc/socket/jsonRpc/TransportWebSocketHandlerTest.java
@@ -0,0 +1,140 @@
+package ru.nikityan.easy.rpc.socket.jsonRpc;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketMessage;
+import ru.nikityan.easy.rpc.socket.Message;
+import ru.nikityan.easy.rpc.socket.MessageChannel;
+import ru.nikityan.easy.rpc.socket.SubscribeMessageChanel;
+import ru.nikityan.easy.rpc.socket.TestWebSocketSession;
+import ru.nikityan.easy.rpc.socket.support.MessageBuilder;
+import ru.nikityan.easy.rpc.socket.support.MessageHeaderAccessor;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Created by Nikit on 02.10.2018.
+ */
+public class TransportWebSocketHandlerTest {
+
+    private final Gson gson = new GsonBuilder().create();
+
+    @Mock
+    private MessageChannel outChanel;
+
+    @Mock
+    private SubscribeMessageChanel inChanel;
+
+    @InjectMocks
+    private TransportWebSocketHandler handler;
+
+    private TestWebSocketSession socketSession = new TestWebSocketSession();
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        socketSession.setId("123");
+        socketSession.setOpen(true);
+
+        handler.afterConnectionEstablished(socketSession);
+    }
+
+    @Test
+    public void handleNotificationNotSubscribe() throws Exception {
+        Message<JsonRpcNotification> build = notification(null, "method");
+        handler.handleMessage(build);
+
+        assertTrue(socketSession.getSentMessages().size() == 0);
+    }
+
+    @Test
+    public void handleNotificationSubscribe() throws Exception {
+        Message<JsonRpcNotification> notification = notification("method", null);
+        handler.handleMessage(notification);
+
+        Message<JsonRpcNotification> notificationMessage = notification(null, "method");
+        handler.handleMessage(notificationMessage);
+
+        assertEquals(socketSession.getSentMessages().size(), 2);
+    }
+
+    @Test
+    public void handleMethodResult() throws Exception {
+        Message<JsonRpcNotification> notification = notification(null, null);
+        handler.handleMessage(notification);
+
+        assertEquals(socketSession.getSentMessages().size(), 1);
+    }
+
+    @Test
+    public void handleNotParse() throws Exception {
+        String noParse = "{ ()foo}";
+        TextMessage message = new TextMessage(noParse);
+
+        handler.handleMessage(socketSession, message);
+
+        WebSocketMessage<?> socketMessage = socketSession.getSentMessages().get(0);
+        String payload = (String) socketMessage.getPayload();
+        JsonRpcResponse response = gson.fromJson(payload, JsonRpcResponse.class);
+
+        assertNotNull(response.getError());
+    }
+
+    @Test
+    public void handleBadRequest() throws Exception {
+        JsonRpcRequest rpcRequest = new JsonRpcRequest(1L, null, null);
+        String json = gson.toJson(rpcRequest);
+        TextMessage message = new TextMessage(json);
+
+        handler.handleMessage(socketSession, message);
+
+        WebSocketMessage<?> socketMessage = socketSession.getSentMessages().get(0);
+        String payload = (String) socketMessage.getPayload();
+        JsonRpcResponse response = gson.fromJson(payload, JsonRpcResponse.class);
+
+        assertNotNull(response.getError());
+    }
+
+    @Test
+    public void correctRequest() throws Exception {
+        JsonRpcRequest rpcRequest = new JsonRpcRequest(1L, "method", null);
+        String json = gson.toJson(rpcRequest);
+        TextMessage message = new TextMessage(json);
+
+        ArgumentCaptor<Message> arg = ArgumentCaptor.forClass(Message.class);
+
+        handler.handleMessage(socketSession, message);
+
+        verify(outChanel).send(arg.capture());
+
+        Message<?> value = arg.getValue();
+        JsonRpcRequest jsonRpcRequest = (JsonRpcRequest) value.getPayload();
+
+        assertEquals(jsonRpcRequest.getMethod(), "method");
+        assertEquals((long) value.getMessageHeader().getId(), 1L);
+    }
+
+    private Message<JsonRpcNotification> notification(String subscribe, String sendMethod) {
+        JsonRpcNotification notification = new JsonRpcNotification("method", null);
+        MessageHeaderAccessor accessor = MessageHeaderAccessor.ofHeaders(null);
+        accessor.setSendMessageMethod(sendMethod);
+        accessor.setMessageMethod("method");
+        accessor.setSessionId("123");
+        if (subscribe != null) {
+            accessor.setSubscribeName(subscribe);
+        }
+
+        return MessageBuilder.fromPayload(notification)
+                .withHeaders(accessor.getMessageHeaders())
+                .build();
+    }
+}
