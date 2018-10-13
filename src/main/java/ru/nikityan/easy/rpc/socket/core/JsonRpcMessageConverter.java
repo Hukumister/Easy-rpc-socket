24e176e8eb22d14b41dff20e0ca16b7da771b548
nmzaltsman <Zaltsman.Nikita@gmail.com>
2018-10-13 17:52:17 +0700

    * fix bug with JsonRpcMessageConverter

--- src/main/java/ru/nikityan/easy/rpc/socket/core/JsonRpcMessageConverter.java
+++ src/main/java/ru/nikityan/easy/rpc/socket/core/JsonRpcMessageConverter.java
@@ -12,35 +12,58 @@ import ru.nikityan.easy.rpc.socket.support.MessageBuilder;
 import ru.nikityan.easy.rpc.socket.support.MessageHeaderAccessor;
 
 /**
- * Created by Nikit on 01.10.2018.
+ * @author CodeRedWolf
+ * @since 1.0
  */
-public class JsonRpcMessageConverter implements MessageConverter {
+public class JsonRpcMessageConverter implements SmartMessageConverter {
 
     private final Gson gson = new GsonBuilder().create();
 
     @Override
     public Object fromMessage(Message<?> message, Class<?> targetClass) {
+        return fromMessage(message, targetClass, null);
+    }
+
+    @Override
+    public Message<?> toMessage(Object payload, @Nullable MessageHeaders headers) {
+        return toMessage(payload, headers, null);
+    }
+
+    @Override
+    public Object fromMessage(Message<?> message, Class<?> targetClass, @Nullable Object conversionHint) {
         Object payload = message.getPayload();
         return gson.fromJson(payload.toString(), targetClass);
     }
 
     @Override
-    public Message<?> toMessage(Object payload, @Nullable MessageHeaders headers) {
-        if (headers != null) {
-            MessageType messageType = headers.getMessageType();
-            if (messageType == MessageType.NOTIFICATION) {
-                String messageMethod = MessageHeaderAccessor.getMessageMethod(headers);
-                JsonRpcNotification notification = new JsonRpcNotification(messageMethod, payload);
-                return MessageBuilder.fromPayload(notification)
-                        .withHeaders(headers)
-                        .build();
-            } else if (messageType == MessageType.RESPONSE) {
-                JsonRpcResponse rpcResponse = new JsonRpcResponse(headers.getId(), payload);
-                return MessageBuilder.fromPayload(rpcResponse)
-                        .withHeaders(headers)
-                        .build();
+    public Message<?> toMessage(Object payload, @Nullable MessageHeaders headers, @Nullable Object conversionHint) {
+        if (headers == null) {
+            return MessageBuilder.fromPayload(payload).build();
+        }
+        MessageType messageType = headers.getMessageType();
+        if (messageType == MessageType.NOTIFICATION) {
+            String messageMethod = MessageHeaderAccessor.getMessageMethod(headers);
+            if (messageMethod == null) {
+                messageMethod = getMessageMethod(conversionHint);
             }
+            JsonRpcNotification notification = new JsonRpcNotification(messageMethod, payload);
+            return MessageBuilder.fromPayload(notification)
+                    .withHeaders(headers)
+                    .build();
+        } else if (messageType == MessageType.RESPONSE) {
+            JsonRpcResponse rpcResponse = new JsonRpcResponse(headers.getId(), payload);
+            return MessageBuilder.fromPayload(rpcResponse)
+                    .withHeaders(headers)
+                    .build();
         }
         return MessageBuilder.fromPayload(payload).withHeaders(headers).build();
+    }
+
+    @Nullable
+    private String getMessageMethod(Object conversionHint) {
+        if (conversionHint != null && conversionHint instanceof String) {
+            return (String) conversionHint;
+        }
+        return null;
     }
 }
